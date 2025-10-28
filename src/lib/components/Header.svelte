8029cfdec20229f58bfb8249bfac0cf3232e904f
Hisashi Suzuki <iya7643@gmail.com>
2025-10-28 18:18:22 +0900

    修正漏れ

--- src/lib/components/Header.svelte
+++ src/lib/components/Header.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+	import { base } from '$app/paths';
 	import { page } from '$app/stores';
 	import { APP_NAME, APP_VERSION } from '$lib/constants';
 	import { isAnnounceEnabled } from '$lib/stores';
@@ -9,7 +10,7 @@
 <nav class="navbar is-fixed-top" style="border-bottom: thin solid darkgray;">
 	<div class="navbar-brand w-100 px-3 is-flex is-justify-content-space-between is-align-items-center">
 		<p class="title is-4 mb-0">
-			<a href="/">{APP_NAME}</a>
+			<a href="{base}/">{APP_NAME}</a>
 			<span class="subtitle is-6 pl-1">v{APP_VERSION}</span>
 		</p>
 
